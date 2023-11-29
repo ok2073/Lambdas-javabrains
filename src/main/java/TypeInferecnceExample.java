c28b19274b495da1215dc0727cd70da7ce188e0d
Marco Scibetta <marco.scibetta@eng.it>
2023-11-29 15:16:11 +0100

    add type inference

---
+++ src/main/java/TypeInferecnceExample.java
@@ -0,0 +1,13 @@
+public class TypeInferecnceExample {
+    public static void main(String[] args) {
+        printLambda(s -> s.length());
+    }
+
+    private static void printLambda(StringLengthLambda l) {
+        System.out.println(l.getLength("Hello Lambda"));
+    }
+}
+
+interface StringLengthLambda {
+    int getLength(String s);
+}
