62f52305155737ee59149ca05012d1b9015594b7
Marco Scibetta <marco.scibetta@eng.it>
2023-12-06 17:08:55 +0100

    add closure example

---
+++ src/main/java/com/keelient/unit2/ClosureExample.java
@@ -0,0 +1,23 @@
+package com.keelient.unit2;
+
+public class ClosureExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        doProcess(a, new Process() {
+            @Override
+            public void process(int i) {
+                System.out.println(i + b);
+            }
+        });
+
+        doProcess(a, i -> System.out.println(i + b));
+    }
+    public static void doProcess(int i, Process process) {
+        process.process(i);
+    }
+}
+
+interface Process {
+    void process(int i);
+}
